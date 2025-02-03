32f0369b4d0eebcc60ae44b08bb15f73098251b6
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-02-04 00:46:19 +0900

    とりあえずなんとなくコンポーネントを置けるように

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { h1Style } from "./styles";
 
-createRoot(document.getElementById("root")!).render(
-  <h1 className="bg-black">Hello, React!</h1>,
-);
+import { AutoHeightTextarea } from "./auto-height-textarea/AutoHeightTextarea";
+
+const App = () => {
+  return (
+    <div className="flex h-full flex-col bg-gray-800 p-4">
+      <h1 className={h1Style}>Auto-resizing textarea</h1>
+      <AutoHeightTextarea />
+    </div>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(<App />);
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,7 @@
 import clsx from "clsx/lite";
 
+export const h1Style = clsx("mb-4 text-xl font-bold text-gray-100");
+
 export const inputStyle = clsx(
-  "rounded-md border border-gray-300 px-1 focus:border-blue-300",
+  "rounded-md border border-gray-300 px-1 outline-none focus:outline-blue-500",
 );
