79e0254f1a825f1a3206a81b2b0c32f0c86e1a3f
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-02-04 01:57:07 +0900

    [tag-suggestion] とりあえずタグを表示

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import "./index.css";
 import { h1Style } from "./styles";
 
 import { AutoHeightTextarea } from "./auto-height-textarea/AutoHeightTextarea";
+import { TagDisplay } from "./tag-suggestion/Tag";
 
 const App = () => {
   return (
-    <div className="flex h-full min-h-screen flex-col bg-gray-800 p-4">
-      <h1 className={h1Style}>Auto-resizing textarea</h1>
-      <AutoHeightTextarea />
+    <div className="flex h-full min-h-screen flex-col gap-4 bg-gray-900 p-4">
+      <section>
+        <h1 className={h1Style}>Auto-resizing textarea</h1>
+        <AutoHeightTextarea />
+      </section>
+      <section>
+        <h1 className={h1Style}>Tag</h1>
+        <TagDisplay tags={["a", "b", "c"]} onClickTag={() => {}} />
+      </section>
     </div>
   );
 };
