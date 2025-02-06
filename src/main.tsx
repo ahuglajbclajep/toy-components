f50099acc8550f003693eeca70692a3274b2ff3f
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-02-07 02:33:13 +0900

    [stories] 余白を大きく取るデザインに

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,24 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { h1Style } from "./styles";
 
 import { AutoResizingTextarea } from "./auto-resizing-textarea/AutoResizingTextarea";
 import { TagSearchInput } from "./tag-search-input";
+import { h1Style, h2Style, sectionStyle } from "./styles";
 
 const App = () => {
   return (
-    <div className="flex h-full min-h-screen flex-col gap-4 bg-gray-900 p-4">
-      <section>
-        <h1 className={h1Style}>Auto-resizing textarea</h1>
-        <AutoResizingTextarea />
-      </section>
-      <section>
-        <h1 className={h1Style}>Tag search input</h1>
-        <TagSearchInput />
-      </section>
+    <div className="h-full min-h-screen bg-gray-900 p-8">
+      <div className="flex flex-col gap-8">
+        <h1 className={h1Style}>Inputs</h1>
+        <section className={sectionStyle}>
+          <h2 className={h2Style}>Auto-resizing textarea</h2>
+          <AutoResizingTextarea />
+        </section>
+        <section className={sectionStyle}>
+          <h2 className={h2Style}>Tag search input</h2>
+          <TagSearchInput />
+        </section>
+      </div>
     </div>
   );
 };
