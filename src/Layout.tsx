b2666c379da7a594e44ce87dc60b6aa1abe8b037
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-02-11 00:13:14 +0900

    [stories] ページに目次を追加

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,39 @@
-import { Outlet } from "react-router";
+import { useMemo } from "react";
+import { Outlet, Link, useParams } from "react-router";
+import clsx from "clsx/lite";
+
+import { h2Style } from "./styles";
 
 export const Layout = () => {
+  const paths = useMemo(() => {
+    const filePaths = Object.keys(import.meta.glob("./stories/*.tsx"));
+    return filePaths.map((path) => path.split(/[/.]/).slice(-2)[0]);
+  }, []);
+
+  const { stories } = useParams();
+
   return (
-    <div className="h-full min-h-screen bg-gray-900 p-8">
-      <Outlet />
+    <div className="bg-gray-900 font-sans text-gray-100">
+      <div className="mx-auto flex h-full min-h-screen max-w-screen-lg gap-4 p-8">
+        <aside className="flex w-40 flex-col gap-4">
+          {paths.map((path) => (
+            <Link
+              key={path}
+              to={path}
+              className={clsx(
+                h2Style,
+                "hover:underline",
+                stories === path && "underline",
+              )}
+            >
+              {path}
+            </Link>
+          ))}
+        </aside>
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,8 @@
 import clsx from "clsx/lite";
 
+export const h1Style = clsx("text-3xl font-bold text-gray-100");
+export const h2Style = clsx("text-xl text-gray-100");
+
 export const inputStyle = clsx(
   "rounded-md border border-gray-500 bg-gray-100 px-1 outline-none focus-within:outline-blue-500",
 );
