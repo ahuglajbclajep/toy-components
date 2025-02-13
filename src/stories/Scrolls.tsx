9efbdfc0982e124182980e0eb57885eae37f62a9
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-02-14 01:27:16 +0900

    Scrolls.tsx を追加

---
+++ src/stories/Scrolls.tsx
@@ -0,0 +1,11 @@
+import { iframeStyle } from "../utils";
+
+export const BidirectionalEndlessScrolling = () => {
+  return (
+    <iframe
+      src="https://ahuglajbclajep.github.io/react-bi-directional-endless-scrolling/"
+      height={800}
+      className={iframeStyle}
+    />
+  );
+};
