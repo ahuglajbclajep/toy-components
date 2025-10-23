3e0358e6d372436a50be9cd092f9ad67751bd77e
ahuglajbclajep <ahuglajbclajep@gmail.com>
2025-10-24 05:48:06 +0900

    README を更新

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This application consists of the following two parts:
 - `main.tsx` and `stories/`<br/>
   A system that provides simple sample pages based on the filenames in `stories/`.
   By writing `.tsx` files in a format similar to [Component Story Format](https://storybook.js.org/docs/api/csf) (note: not compatible), you can easily create a component catalog.
+  Inspired by [myuon/mu-ui](https://github.com/myuon/mu-ui/tree/main/uibook), but each page is loaded individually via dynamic import.
 - Components displayed in `stories/` (e.g. `AutoResizingTextarea.tsx`)<br/>
   A proof of concept (PoC) for slightly complex React components.
   Includes components implemented in other repositories.
--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -7,7 +7,8 @@ export const Welcome = () => {
         <ul className="list-disc space-y-4 p-4">
           <li>
             <p>
-              <Code>main.tsx</Code> and <Code>stories/</Code>
+              <Code>main.tsx</Code> and{" "}
+              <GhCodeLink path="src/stories">stories/</GhCodeLink>
             </p>
             <p>
               A system that provides simple sample pages based on the filenames
@@ -17,12 +18,20 @@ export const Welcome = () => {
                 Component Story Format
               </ExLink>{" "}
               (note: not compatible), you can easily create a component catalog.
+              Inspired by{" "}
+              <ExLink href="https://github.com/myuon/mu-ui/tree/main/uibook">
+                myuon/mu-ui
+              </ExLink>
+              , but each page is loaded individually via dynamic import.
             </p>
           </li>
           <li>
             <p>
               Components displayed in <Code>stories/</Code> (e.g.{" "}
-              <Code>AutoResizingTextarea.tsx</Code>)
+              <GhCodeLink path="src/auto-resizing-textarea">
+                AutoResizingTextarea.tsx
+              </GhCodeLink>
+              )
             </p>
             <p>
               A proof of concept (PoC) for slightly complex React components.
@@ -45,8 +54,22 @@ const Code = ({ children }: { children: string }) => (
   <code className="rounded-xs bg-neutral-100 px-0.5">{children}</code>
 );
 
-const ExLink = ({ href, children }: { href: string; children: string }) => (
+const ExLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
   <a href={href} target="_blank" className="underline">
     {children}
   </a>
+);
+
+const GhCodeLink = ({ path, children }: { path: string; children: string }) => (
+  <ExLink
+    href={`https://github.com/ahuglajbclajep/toy-components/tree/main/${path}`}
+  >
+    <Code>{children}</Code>
+  </ExLink>
 );
