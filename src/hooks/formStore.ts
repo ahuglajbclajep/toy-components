c68cd9e15b102d4ec68f44859799e8bd3479ee18
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-02-27 00:13:46 +0900

    フックをクラスで書き換える

--- src/hooks/formStore.ts
+++ src/hooks/formStore.ts
@@ -1,44 +1,31 @@
-import {
-  createContext,
-  useCallback,
-  useContext,
-  useRef,
-  type RefCallback,
-} from "react";
+import { createContext, useContext, type RefCallback } from "react";
 
 type FieldName = "nickname" | "gender";
 type FieldElement = HTMLInputElement | HTMLSelectElement;
 
-type Register = (name: FieldName) => RefCallback<FieldElement>;
-export const FormContext = createContext<Register | null>(null);
+export const FormContext = createContext<FormStore | null>(null);
 
-export const useForm = () => {
-  const fields = useRef(new Map<FieldName, FieldElement>());
+export class FormStore {
+  private fields = new Map<FieldName, FieldElement>();
 
-  const register = useCallback<Register>(
-    (name) => (instance) => {
-      instance
-        ? fields.current.set(name, instance)
-        : fields.current.delete(name);
-    },
-    [],
-  );
+  register =
+    (name: FieldName): RefCallback<FieldElement> =>
+    (instance) => {
+      instance ? this.fields.set(name, instance) : this.fields.delete(name);
+    };
 
-  const getValues = useCallback(
-    () => ({
-      nickname: fields.current.get("nickname")?.value,
-      gender: fields.current.get("gender")?.value,
-    }),
-    [],
-  );
-
-  return { register, getValues };
-};
+  getValues = () => {
+    return {
+      nickname: this.fields.get("nickname")?.value,
+      gender: this.fields.get("gender")?.value,
+    };
+  };
+}
 
 export const useFormRegister = () => {
-  const registerRef = useContext(FormContext);
-  if (!registerRef) {
+  const context = useContext(FormContext);
+  if (!context) {
     throw new Error("useFormRegister must be used within a FormContext");
   }
-  return registerRef;
+  return context;
 };
