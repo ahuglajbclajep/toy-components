179c33840f4c14ae740b8c53f2689803a9919af7
ahuglajbclajep <ahuglajbclajep@gmail.com>
2026-04-01 10:45:02 +0900

    細かい部分を調整

--- src/utils.ts
+++ src/utils.ts
@@ -8,11 +8,13 @@ export const textInputStyle = clsx(
   baseStyle,
   "cursor-text placeholder:text-text-muted",
   "focus-within:border-focus focus-within:outline focus-within:outline-focus",
+  "aria-invalid:border-danger-muted aria-invalid:outline aria-invalid:outline-danger-muted",
 );
 
 export const buttonStyle = clsx(
   baseStyle,
   "cursor-pointer transition-colors hover:bg-bg-muted active:bg-bg-emphasis",
+  "disabled:cursor-not-allowed disabled:bg-bg-muted",
 );
 
 export const iconLikeStyle = clsx(
